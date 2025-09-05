2faed045727695a347b57197a5b32b643fc86a58
Nullskulls <nullskulls@proton.me>
2025-09-05 22:00:14 +0300

    forgot the / in the api path...

--- Source/app.py
+++ Source/app.py
@@ -15,24 +15,24 @@ def require_api_key():
     if request.headers.get("key") != auth["key"]:
         return jsonify({"bitchless": False, "error": "get fucked"}), 401
 
-@app.post("api/v1/tickets/close/<ticket_id>")
+@app.post("/api/v1/tickets/close/<ticket_id>")
 def collapse_ticket(ticket_id):
     close_ticket(ticket_id)
     return jsonify({"ok": True})
 
-@app.get("api/v1/tickets/open")
+@app.get("/api/v1/tickets/open")
 def get_o_tickets():
     return jsonify({"tickets": get_open_tickets(), "ok": True})
 
-@app.get("api/v1/tickets/all")
+@app.get("/api/v1/tickets/all")
 def get_a_tickets():
     return jsonify({"tickets": get_all_tickets(), "ok": True})
 
-@app.get("api/v1/tickets/closed")
+@app.get("/api/v1/tickets/closed")
 def get_c_tickets():
     return jsonify({"tickets": get_closed_tickets(), "ok": True})
 
-@app.get("api/v1/health")
+@app.get("/api/v1/health")
 def get_health():
     result = subprocess.run(
         ["systemctl", "--user", "is-active", "vmpheus"],
